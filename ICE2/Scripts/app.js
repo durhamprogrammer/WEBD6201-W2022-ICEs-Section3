eff1a8eae5da5030111b9451f209dc80cc868797
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-01-21 09:49:47 -0500

    inserted Dom Elements into index.html

--- ICE2/Scripts/app.js
+++ ICE2/Scripts/app.js
@@ -10,6 +10,26 @@
         {
             location.href = "about.html";
         });
+
+        // Step 1. get an entry point(s) (insertion point / deletion point) reference 
+        let DocumentBody = document.body;
+        let MainContent = document.getElementsByTagName("main")[0];
+
+        // Step 2. create an element(s) to insert
+        let MainParagraph = document.createElement("p");
+        let Article = document.createElement("article");
+        let ArticleParagraph = `<p id="ArticleParagraph" class ="mt-3">This is the Article Paragraph</p>`
+
+        // Step 3. configure the new element
+        MainParagraph.setAttribute("id", "MainParagraph");
+        MainParagraph.setAttribute("class", "mt-3");
+        MainParagraph.textContent = "This is the Main Paragraph";
+        Article.setAttribute("class", "container");
+
+        // Step 4. Add / Insert the new element
+        MainContent.appendChild(MainParagraph);
+        Article.innerHTML = ArticleParagraph;
+        DocumentBody.appendChild(Article);
     }
 
     function DisplayProductsPage()
