614266a28b92036c09ed7a4aa2dd8ad41c048ab7
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-03-09 16:10:45 -0500

    converted authguard.js to ts

--- ICE8/Scripts/authguard.js
+++ ICE8/Scripts/authguard.js
@@ -1,19 +1,12 @@
 "use strict";
-((function()
-{
+((function () {
     let protected_routes = [
         "contact-list"
     ];
-
-
-    if(protected_routes.indexOf(router.ActiveLink) > -1)
-    {
-        // check if user is logged in
-        if(!sessionStorage.getItem("user"))
-        {
-            // if not...redirect them back to the login page
+    if (protected_routes.indexOf(router.ActiveLink) > -1) {
+        if (!sessionStorage.getItem("user")) {
             location.href = "/login";
         }
     }
-   
 }))();
+//# sourceMappingURL=authguard.js.map
