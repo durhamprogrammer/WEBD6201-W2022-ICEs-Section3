0a071f5818147a34c5cf109e3cb55bbb61507464
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-03-16 16:59:26 -0400

    converted some functions to arrow

--- ICE9/server.js
+++ ICE9/server.js
@@ -2,12 +2,12 @@ import { getData } from './users.data.js';
 import { sayHello, sayGoodbye } from './hello.js';
 
 getData()
-  .then(function (data) {
+  .then((data) => {
     sayHello();
     console.log(data);
     sayGoodbye();
   })
-  .catch(function (err) {
-    console.error("ERROR: User data not returned!");
+  .catch((err) => {
+    console.error("ERROR: User data not returned!: "  + err.message);
   });
 
--- ICE9/users.data.js
+++ ICE9/users.data.js
@@ -1,16 +1,13 @@
 import { promises } from 'fs';
 const fsPromises = promises;
 
-export async function getData()
-{
-    return await fsPromises.readFile("./Data/users.json", "utf8", function(err, data)
-    {
-        if(err)
-        {
-            console.error(err.message);
-        }
-        return data;
-    });
+export async function getData() {
+    return await fsPromises.readFile("./Data/users.json", "utf8", (err, data) => {
+            if (err) {
+                console.error(err.message);
+            }
+            return data;
+        });
 }
 
 
