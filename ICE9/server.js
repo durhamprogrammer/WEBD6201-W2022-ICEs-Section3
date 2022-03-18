def568d7a52b75375df1bde39ae261140565ef5e
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-03-18 09:52:45 -0400

    fixed server and other bugs

--- ICE9/server.js
+++ ICE9/server.js
@@ -5,18 +5,27 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const http_1 = __importDefault(require("http"));
 const fs_1 = __importDefault(require("fs"));
-const hostname = '127.0.0.1';
+const mime_types_1 = __importDefault(require("mime-types"));
+const hostname = 'localhost';
 const port = 3000;
+let lookup = mime_types_1.default.lookup;
 const server = http_1.default.createServer((req, res) => {
-    let path = __dirname + req.url;
-    fs_1.default.readFile(path, function (err, data) {
+    let parsedURL = new URL(req.url, "http://" + hostname + ":" + port);
+    let path = parsedURL.pathname.replace(/^\/+|\/+$/g, "");
+    if (path == "") {
+        path = "index.html";
+    }
+    let file = __dirname + "\\" + path;
+    fs_1.default.readFile(file, function (err, content) {
         if (err) {
             res.writeHead(404);
             res.end(JSON.stringify(err));
             return;
         }
-        res.writeHead(200);
-        res.end(data);
+        res.setHeader("X-Content-Type-Options", "nosniff");
+        let mimeType = lookup(path);
+        res.writeHead(200, "", { "Content-Type": mimeType });
+        res.end(content);
     });
 });
 server.listen(port, hostname, function () {
--- ICE9/server.ts
+++ ICE9/server.ts
@@ -1,22 +1,35 @@
 import http from 'http';
 import fs from 'fs';
+import url from 'url';
+import mime from 'mime-types';
 
-const hostname: string = '127.0.0.1';
+const hostname: string = 'localhost';
 const port: number = 3000;
+let lookup = mime.lookup; // alias for mime.lookup
 
 // create a server object (Immutable)
-const server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => 
+const server = http.createServer((req, res) => 
 {
-  let path =  __dirname + req.url;
-  fs.readFile(path, function (err, data) {
+  let parsedURL = new URL(req.url as string, "http://" + hostname + ":" + port);
+  let path = parsedURL.pathname.replace(/^\/+|\/+$/g, "");
+
+  if (path == "") {
+    path = "index.html";
+  }
+
+  let file = __dirname + "\\" + path;
+
+  fs.readFile(file, function (err, content) {
     if (err) 
     {
       res.writeHead(404); // file not found
       res.end(JSON.stringify(err));
       return;
     }
-    res.writeHead(200);
-    res.end(data);
+    res.setHeader("X-Content-Type-Options", "nosniff");
+    let mimeType = lookup(path) as string;
+    res.writeHead(200, "", { "Content-Type": mimeType });
+    res.end(content);
   });
 });
 
@@ -25,3 +38,4 @@ server.listen(port, hostname, function()
 {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
